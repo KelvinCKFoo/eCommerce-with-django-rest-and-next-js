70241d5a5b0ecf3bf58916490c5d837a02fe9ecc
KelvinCKFoo <kelvinckfoo@gmail.com>
2025-02-07 18:24:12 +0000

    GET PUT DELETE

--- newproject/api/urls.py
+++ newproject/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import get_products,enter_product
+from .views import get_products,enter_product,product_detail
 
 urlpatterns = [path('products/', get_products,name='get_products'),
-               path('product/enter', enter_product, name='enter_product')
-               
+               path('product/enter', enter_product, name='enter_product'),
+               path('product/<int:pk>', product_detail, name='product_detail')
                
                
                
